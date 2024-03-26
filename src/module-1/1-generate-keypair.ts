f2588bfcbe777c12c75e4db7e8a3272103261a8f
aciDrums7 <edoardo.carradori.96@gmail.com>
2024-03-26 08:19:11 +0000

    Cryptography fundamentals

---
+++ src/module-1/1-generate-keypair.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+import { getKeypairFromEnvironment } from '@solana-developers/helpers'
+import { Keypair } from '@solana/web3.js'
+
+const generatedKeypair = Keypair.generate()
+
+console.log(`✅ Generated keypair!`)
+console.log(`The generated public key is: `, generatedKeypair.publicKey.toBase58())
+console.log(`The generated private key is: `, uint8ArrayToHex(generatedKeypair.secretKey))
+
+function uint8ArrayToHex(uint8Array: Uint8Array) {
+    return Array.from(uint8Array)
+        .map((byte) => byte.toString(16).padStart(2, '0'))
+        .join('')
+}
+
+const restoredKeypair = getKeypairFromEnvironment('PRIVATE_KEY')
+
+console.log(`✅ Finished! We've loaded our secret key securely, using an env file!`)
+console.log(`The restored public key is: `, restoredKeypair.publicKey.toBase58())
+console.log(`The restored private key is: `, uint8ArrayToHex(restoredKeypair.secretKey))
